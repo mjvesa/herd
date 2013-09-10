5e6cd624bc2f3901bfa8b718892cd2aa86010c93
Matti Vesa <mjvesa@vaadin.com>
2013-09-10 16:06:24 +0300

    #9 Refactor ForthIDE

--- src/main/java/com/github/mjvesa/f4v/ForthIDE.java
+++ src/main/java/com/github/mjvesa/f4v/ForthIDE.java
@@ -19,12 +19,12 @@ import org.vaadin.aceeditor.AceEditor;
 import org.vaadin.aceeditor.AceMode;
 import org.vaadin.aceeditor.AceTheme;
 
-import com.vaadin.data.Property;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.HorizontalSplitPanel;
@@ -41,177 +41,237 @@ import com.vaadin.ui.VerticalSplitPanel;
  * @author mjvesa@vaadin.com
  * 
  */
-public class ForthIDE extends HorizontalSplitPanel implements View  {
+public class ForthIDE extends HorizontalSplitPanel implements View {
 
-    /**
+	/**
      * 
      */
-    private static final long serialVersionUID = -6911850470039819669L;
+	private static final long serialVersionUID = -6911850470039819669L;
 
-    private TabSheet editorAndLayout;
+	private TextArea console;
+	private StringBuffer consoleString;
+	private Panel panel;
+	private ListSelect blockSelect;
+	private TextField bufferName;
+	private Button clearConsoleButton;
+	private ListSelect wordListSelect;
+	private AceEditor editor;
 
-    private TextArea console;
-    private StringBuffer consoleString;
+	private Interpreter interpreter;
 
-    private Panel panel;
+	public ForthIDE() {
+		setSizeFull();
+		addComponent(constructBuffersAndStatus());
+		addComponent(constructEditorAndLayoutPanel());
+		setSplitPosition(15);
 
-    private TabSheet buffersAndStatus;
-    private ListSelect bufferSelect;
-    private TextField bufferName;
-    private Button saveBlockButton;
-    private Button clearConsoleButton;
+		interpreter = new Interpreter();
+		interpreter.setView(this);
+		interpreter.setup();
+		fillBufferSelect();
+	}
 
-    private Button printStackButton;
-    private ListSelect wordListSelect;
+	/**
+	 * Fills the select for buffers with their filenames
+	 */
+	private void fillBufferSelect() {
+		for (String s : interpreter.getBufferList()) {
+			blockSelect.addItem(s);
+		}
+	}
 
-    private AceEditor editor;
+	private TabSheet constructBuffersAndStatus() {
+		TabSheet tabs = new TabSheet();
+		tabs.setSizeFull();
+		tabs.addTab(constructBuffersTab(), "Buffers", null);
+		tabs.addTab(constructStatusTab(), "Status", null);
+		return tabs;
+	}
 
-    private Interpreter interpreter;
+	private Component constructStatusTab() {
+		VerticalLayout vl = new VerticalLayout();
+		vl.setSizeFull();
+		vl.addComponent(createPrintStackButton());
+		wordListSelect = createWordListSelect();
+		vl.addComponent(wordListSelect);
+		vl.setExpandRatio(wordListSelect, 1);
+		return vl;
+	}
 
-    public ForthIDE() {
-        setSizeFull();
-        constructBuffersAndStatus();
-        constructEditorAndLayout();
-        interpreter = new Interpreter();
-        interpreter.setView(this);
-        interpreter.setup();
-        fillBufferSelect();
-    }
+	private Component constructBuffersTab() {
+		VerticalLayout vl = new VerticalLayout();
+		vl.setSpacing(true);
+		vl.setSizeFull();
+		vl.addComponent(constructBufferNameAndSaveButton());
+		blockSelect = createBlockSelect();
+		vl.addComponent(blockSelect);
+		vl.setExpandRatio(blockSelect, 1);
+		return vl;
+	}
 
-    /**
-     * Fills the select for buffers with their filenames
-     */
-    private void fillBufferSelect() {
-        for (String s : interpreter.getBufferList()) {
-            bufferSelect.addItem(s);
+	private Component constructBufferNameAndSaveButton() {
 
-        }
-    }
+		HorizontalLayout hl = new HorizontalLayout();
+		hl.setSpacing(true);
+		bufferName = createBufferName();
+		hl.addComponent(bufferName);
+		hl.addComponent(createSaveBlockButton());
+		return hl;
+	}
 
-    private void constructBuffersAndStatus() {
-        buffersAndStatus = new TabSheet();
-        buffersAndStatus.setSizeFull();
+	private ListSelect createWordListSelect() {
+		ListSelect ls = new ListSelect("Wordlist");
+		ls.setSizeFull();
+		ls.addStyleName("wordlist");
+		return ls;
+	}
 
-        VerticalLayout vl = new VerticalLayout();
-        vl.setSpacing(true);
-        vl.setSizeFull();
-        HorizontalLayout hl = new HorizontalLayout();
-        hl.setSpacing(true);
+	private Component createPrintStackButton() {
+		Button b = new Button("printStack", new Button.ClickListener() {
 
-        bufferName = new TextField();
-        saveBlockButton = new Button("Save", new Button.ClickListener() {
-            /**
-		     * 
-		     */
-            private static final long serialVersionUID = -1273939649837923281L;
-
-            public void buttonClick(ClickEvent event) {
-                String name = (String) bufferName.getValue();
-                String code = (String) editor.getValue();
-                interpreter.addSource(name, code);
-                bufferSelect.addItem(name);
-            }
-        });
-        hl.addComponent(bufferName);
-        hl.addComponent(saveBlockButton);
-        vl.addComponent(hl);
-
-        bufferSelect = new ListSelect("Blocks");
-        bufferSelect.setSizeFull();
-        bufferSelect.setNullSelectionAllowed(false);
-        bufferSelect.setImmediate(true);
-        bufferSelect.addValueChangeListener(new ValueChangeListener() {
-
-        /**
+		/**
 	     * 
 	     */
-            private static final long serialVersionUID = 2450173366304389892L;
+			private static final long serialVersionUID = -7954166353532284682L;
 
-            public void valueChange(ValueChangeEvent event) {
-                String value = (String) bufferSelect.getValue();
-                editor.setValue(interpreter.getSource(value));
-                bufferName.setValue(value);
-            }
-
-        });
-
-        vl.addComponent(bufferSelect);
-        vl.setExpandRatio(bufferSelect, 1);
-        buffersAndStatus.addTab(vl, "Buffers", null);
-
-        vl = new VerticalLayout();
-        vl.setSizeFull();
-        printStackButton = new Button("printStack", new Button.ClickListener() {
-
-            /**
-	     * 
-	     */
-            private static final long serialVersionUID = -7954166353532284682L;
-
-            public void buttonClick(ClickEvent event) {
-                interpreter.printStack();
-            }
-
-        });
-        vl.addComponent(printStackButton);
-        wordListSelect = new ListSelect("Wordlist"); // ,
-        // interpreter.getWordList());
-        wordListSelect.setSizeFull();
-        wordListSelect.addStyleName("wordlist");
-        vl.addComponent(wordListSelect);
-        vl.setExpandRatio(wordListSelect, 1);
-        buffersAndStatus.addTab(vl, "Status", null);
-        addComponent(buffersAndStatus);
-
-    }
-
-    private void constructEditorAndLayout() {
-        VerticalLayout vl = new VerticalLayout();
-        editorAndLayout = new TabSheet();
-        editorAndLayout.setSizeFull();
-
-        vl = new VerticalLayout();
-        vl.setSpacing(true);
-        vl.setSizeFull();
-        editor = createEditor();
-        vl.addComponent(editor);
-        vl.setExpandRatio(editor, 1);
-
-        HorizontalLayout hl = new HorizontalLayout();
-        hl.setSpacing(true);
-        Button b = new Button("Execute");
-        b.setImmediate(true);
-        b.addClickListener(runListener);
-        hl.addComponent(b);
-
-        b = new Button("Clear and execute");
-        b.setImmediate(true);
-        b.addClickListener(clearAndRunListener);
-        hl.addComponent(b);
-
-        clearConsoleButton = new Button("Clear console");
-        clearConsoleButton.addClickListener(clearConsoleListener);
-        hl.addComponent(clearConsoleButton);
-
-        CheckBox cb = new CheckBox("log executed words");
-        cb.addValueChangeListener(new ValueChangeListener() {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 8229355957367047681L;
-
-			@Override
-			public void valueChange(ValueChangeEvent event) {
-                interpreter.setLogExecutedWords((Boolean) event.getProperty()
-                        .getValue());
+			public void buttonClick(ClickEvent event) {
+				interpreter.printStack();
 			}
 		});
+		return b;
+	}
 
-        hl.addComponent(cb);
+	private ListSelect createBlockSelect() {
 
-        cb = new CheckBox("log added words");
-        cb.addValueChangeListener(new ValueChangeListener() {
-			
+		final ListSelect ls = new ListSelect("Blocks");
+		ls.setSizeFull();
+		ls.setNullSelectionAllowed(false);
+		ls.setImmediate(true);
+		ls.addValueChangeListener(new ValueChangeListener() {
+
+			/**
+	     * 
+	     */
+			private static final long serialVersionUID = 2450173366304389892L;
+
+			public void valueChange(ValueChangeEvent event) {
+				String value = (String) ls.getValue();
+				editor.setValue(interpreter.getSource(value));
+				bufferName.setValue(value);
+			}
+		});
+		return ls;
+	}
+
+	private Button createSaveBlockButton() {
+
+		Button saveBlockButton = new Button("Save", new Button.ClickListener() {
+			/**
+		     * 
+		     */
+			private static final long serialVersionUID = -1273939649837923281L;
+
+			public void buttonClick(ClickEvent event) {
+				String name = (String) bufferName.getValue();
+				String code = (String) editor.getValue();
+				interpreter.addSource(name, code);
+				blockSelect.addItem(name);
+			}
+		});
+		return saveBlockButton;
+	}
+
+	private TextField createBufferName() {
+		final TextField tf = new TextField();
+		return tf;
+	}
+
+	private VerticalSplitPanel constructEditorAndLayoutPanel() {
+		VerticalSplitPanel vsp = new VerticalSplitPanel();
+		vsp.setSizeFull();
+		vsp.addComponent(constructEditorAndLayout());
+		vsp.setSplitPosition(80);
+		console = createConsole();
+		vsp.addComponent(console);
+		return vsp;
+	}
+
+	private TextArea createConsole() {
+		consoleString = new StringBuffer();
+		TextArea ta = new TextArea();
+		ta.setSizeFull();
+		print("Yes. This is Forth. Welcome!");
+		return ta;
+	}
+
+	private Component constructEditorAndLayout() {
+		TabSheet ts = new TabSheet();
+		ts.setSizeFull();
+		ts.addTab(constructEditorTab(), "Editor", null);
+		ts.addTab(constructOutputTab(), "Output", null);
+		return ts;
+	}
+
+	private Component constructOutputTab() {
+		VerticalLayout vl = new VerticalLayout();
+		vl.setSizeFull();
+		vl.addComponent(createLayoutClearButton());
+		panel = createMainPanel();
+		vl.addComponent(panel);
+		vl.setExpandRatio(panel, 1);
+		return vl;
+	}
+
+	private Component createLayoutClearButton() {
+		return new Button("Clear layout", new Button.ClickListener() {
+			/**
+	     * 
+	     */
+			private static final long serialVersionUID = -7003358831608079032L;
+
+			public void buttonClick(ClickEvent event) {
+				((VerticalLayout) panel.getContent()).removeAllComponents();
+			}
+		});
+	}
+
+	private Panel createMainPanel() {
+		Panel p = new Panel();
+		VerticalLayout vl = new VerticalLayout();
+		vl.setSpacing(true);
+		p.setContent(vl);
+		p.setSizeFull();
+		return p;
+	}
+
+	private Component constructEditorTab() {
+		VerticalLayout vl = new VerticalLayout();
+		vl.setSpacing(true);
+		vl.setSizeFull();
+		editor = createEditor();
+		vl.addComponent(editor);
+		vl.setExpandRatio(editor, 1);
+		vl.addComponent(constructButtons());
+		return vl;
+	}
+
+	private Component constructButtons() {
+
+		HorizontalLayout hl = new HorizontalLayout();
+		hl.setSpacing(true);
+		hl.addComponent(createRunButton());
+		hl.addComponent(createClearAndExecuteButton());
+		hl.addComponent(createClearConsoleButton());
+		hl.addComponent(createLogExecutedWordsCheckBox());
+		hl.addComponent(createLogAddedWordCheckBox());
+
+		return hl;
+	}
+
+	private Component createLogAddedWordCheckBox() {
+		CheckBox cb = new CheckBox("log added words");
+		cb.addValueChangeListener(new ValueChangeListener() {
+
 			/**
 			 * 
 			 */
@@ -219,131 +279,111 @@ public class ForthIDE extends HorizontalSplitPanel implements View  {
 
 			@Override
 			public void valueChange(ValueChangeEvent event) {
-                interpreter.setLogNewWords((Boolean) event.getProperty()
-                        .getValue());
+				interpreter.setLogNewWords((Boolean) event.getProperty()
+						.getValue());
 			}
 		});
+		return cb;
+	}
 
-        hl.addComponent(cb);
+	private Component createLogExecutedWordsCheckBox() {
+		
+		CheckBox cb = new CheckBox("log executed words");
+		cb.addValueChangeListener(new ValueChangeListener() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 8229355957367047681L;
 
-        vl.addComponent(hl);
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				interpreter.setLogExecutedWords((Boolean) event.getProperty()
+						.getValue());
+			}
+		});
+		return cb;
+	}
 
-        editorAndLayout.addTab(vl, "Editor", null);
+	private Component createClearConsoleButton() {
+		return new Button("Clear console",
+				new Button.ClickListener() {
+			/**
+		 * 
+		 */
+			private static final long serialVersionUID = -795270660017061409L;
 
-        vl = new VerticalLayout();
-        vl.setSizeFull();
-        vl.addComponent(new Button("Clear layout", new Button.ClickListener() {
-            /**
-	     * 
-	     */
-            private static final long serialVersionUID = -7003358831608079032L;
+			public void buttonClick(ClickEvent event) {
+				consoleString = new StringBuffer();
+				print("");
+			}
+		});
+	}
 
-            public void buttonClick(ClickEvent event) {
-                ((VerticalLayout)panel.getContent()).removeAllComponents();
-            }
-        }));
+	private Component createClearAndExecuteButton() {
+		return  new Button("Clear and execute",
+				new Button.ClickListener() {
 
-        panel = new Panel();
-        VerticalLayout panelContent = new VerticalLayout();
-        panelContent.setSpacing(true);
-        panel.setContent(panelContent);
-        
-        panel.setSizeFull();
+			/**
+		 * 
+		 */
+			private static final long serialVersionUID = 8556993951867295858L;
 
-        vl.addComponent(panel);
-        vl.setExpandRatio(panel, 1);
+			public void buttonClick(ClickEvent event) {
+				String command = (String) editor.getValue();
+				if (!command.isEmpty()) {
+					((VerticalLayout) panel.getContent()).removeAllComponents();
+					interpreter.runBuffer(command);
+				}
+			}
+		});
+	}
 
-        editorAndLayout.addTab(vl, "Output", null);
+	private Component createRunButton() {
 
-        VerticalSplitPanel vsp = new VerticalSplitPanel();
-        vsp.setSizeFull();
-        vsp.addComponent(editorAndLayout);
-        vsp.setSplitPosition(80);
+		return new Button("Execute", new Button.ClickListener() {
 
-        consoleString = new StringBuffer();
-        console = new TextArea();
-        console.setSizeFull();
-        print("Yes. This is Forth. Welcome!");
-        vsp.addComponent(console);
-        addComponent(vsp);
-        setSplitPosition(15);
+			/**
+    	 * 
+    	 */
+			private static final long serialVersionUID = -4517134025974358295L;
 
-    }
+			public void buttonClick(ClickEvent event) {
+				String command = (String) editor.getValue();
+				if (!command.isEmpty()) {
+					interpreter.runBuffer(command);
+				}
+			}
+		});
+	}
 
-    public AceEditor createEditor() {
-        AceEditor editor = new AceEditor();
-        editor.setMode(AceMode.text);
-        editor.setTheme(AceTheme.vibrant_ink);
-        editor.setSizeFull();
-        return editor;
-    }
+	public AceEditor createEditor() {
+		AceEditor editor = new AceEditor();
+		editor.setMode(AceMode.text);
+		editor.setTheme(AceTheme.vibrant_ink);
+		editor.setSizeFull();
+		return editor;
+	}
 
-    private Button.ClickListener runListener = new Button.ClickListener() {
+	public void print(String msg) {
+		consoleString.insert(0, "\n");
+		consoleString.insert(0, msg);
+		console.setValue(consoleString.toString());
+	}
 
-        /**
-	 * 
-	 */
-        private static final long serialVersionUID = -4517134025974358295L;
+	public void addNewWord(String word) {
+		wordListSelect.addItem(word);
+	}
 
-        public void buttonClick(ClickEvent event) {
-            String command = (String) editor.getValue();
-            if (!command.isEmpty()) {
-                interpreter.runBuffer(command);
-            }
-        }
+	public ComponentContainer getMainComponentContainer() {
+		return (ComponentContainer) panel.getContent();
+	}
 
-    };
+	public void disableContinueButton() {
+		clearConsoleButton.setEnabled(false);
+	}
 
-    private Button.ClickListener clearConsoleListener = new Button.ClickListener() {
-
-        /**
-	 * 
-	 */
-        private static final long serialVersionUID = -795270660017061409L;
-
-        public void buttonClick(ClickEvent event) {
-            consoleString = new StringBuffer();
-            print("");
-        }
-
-    };
-
-    private Button.ClickListener clearAndRunListener = new Button.ClickListener() {
-
-        /**
-	 * 
-	 */
-        private static final long serialVersionUID = 8556993951867295858L;
-
-        public void buttonClick(ClickEvent event) {
-            String command = (String) editor.getValue();
-            if (!command.isEmpty()) {
-                ((VerticalLayout)panel.getContent()).removeAllComponents();
-                interpreter.runBuffer(command);
-            }
-        }
-    };
-
-    public void print(String msg) {
-        consoleString.insert(0, "\n");
-        consoleString.insert(0, msg);
-        console.setValue(consoleString.toString());
-    }
-
-    public void addNewWord(String word) {
-        wordListSelect.addItem(word);
-    }
-
-    public ComponentContainer getMainComponentContainer() {
-        return (ComponentContainer)panel.getContent();
-    }
-
-    public void disableContinueButton() {
-        clearConsoleButton.setEnabled(false);
-    }
-
-    public void enableContinueButton() {
-        clearConsoleButton.setEnabled(true);
-    }
+	public void enableContinueButton() {
+		clearConsoleButton.setEnabled(true);
+	}
 
 }
